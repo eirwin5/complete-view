69c637108b0c6625122759988309fdaeec09c32f
eirwin <eirwin@umich.edu>
2023-11-18 17:02:30 -0500

    finished with recipe card flip and reduced motion

---
+++ js/recipes.js
@@ -0,0 +1,27 @@
+// function flipcard(card) { 
+//     document.getElementById("flip-card-inner").style.transform = 'rotateY(180deg)';
+// }
+
+function showNextItem(olId) {
+    $("#" + olId + " li.active").slideUp("fast", function () {
+      $(this).removeClass("active");
+
+      $(this)
+        .next("li")
+        .slideDown("fast", function () {
+          $(this).addClass("active");
+        });
+    });
+  }
+
+  function goToFirst(olId) {
+    $("#" + olId + " li")
+      .not(":first")
+      .slideUp("fast", function () {
+        $(this).removeClass("active");
+      });
+
+    $("#" + olId + " li:first").slideDown("fast", function () {
+      $(this).addClass("active");
+    });
+  }
